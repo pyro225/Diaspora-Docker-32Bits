ba099176b83bbceab5f767e176926b415ecb5f84
Chocobozzz <florian.bigard@gmail.com>
2016-09-23 09:42:16 +0200

    Update to ruby 2.3

--- with_http_sql/nginx_mysql/install_diaspora.sh
+++ with_http_sql/nginx_mysql/install_diaspora.sh
@@ -6,6 +6,6 @@ j=$(nproc)
 bundle config jobs $j
 bundle config path /home/diaspora/gems
 
-RAILS_ENV=production bin/bundle install --retry 5 --without test development
+RAILS_ENV=production bin/bundle install --retry 5 --without test development --with mysql
 RAILS_ENV=production bin/bundle exec rake db:create db:schema:load
 bundle exec rake assets:precompile
