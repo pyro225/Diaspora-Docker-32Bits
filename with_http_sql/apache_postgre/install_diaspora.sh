c30b87f92af429ae8416a6c0965e3100b67a17c4
Chocobozzz <florian.bigard@gmail.com>
2016-09-23 10:35:49 +0200

    Try to fix turbo assets issue

--- with_http_sql/apache_postgre/install_diaspora.sh
+++ with_http_sql/apache_postgre/install_diaspora.sh
@@ -6,6 +6,6 @@ j=$(nproc)
 bundle config jobs $j
 bundle config path /home/diaspora/gems
 
-RAILS_ENV=production  DB=postgres  bin/bundle install --retry 5 --without test development --with postgresql
-RAILS_ENV=production  DB=postgres  bin/bundle exec rake db:create db:schema:load
-DB=postgres bundle exec rake assets:precompile
+RAILS_ENV=production bin/bundle install --retry 5 --deployment --without test development --with postgresql
+RAILS_ENV=production bin/rake db:create db:schema:load
+RAILS_ENV=production bin/rake assets:precompile
